8458cd965d24770f1af1970d91b4b44348594c72
opilane <opilane@gmail.com>
2023-09-05 16:07:58 +0300

    Initial commit

---
+++ src/main/java/com/example/purjepaus/PurjepausApplication.java
@@ -0,0 +1,13 @@
+package com.example.purjepaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurjepausApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurjepausApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/purjepaus/PurjepausApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.purjepaus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurjepausApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
